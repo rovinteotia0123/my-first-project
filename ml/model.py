9b44530f0e62ebcfe31a2e1210bc3bf916cf2dd2
hiteshkumar54 <hitesh.gla_cs.aiml24@gla.ac.in>
2026-03-14 16:10:49 +0530

    Improved ML prediction with input validation

--- ml/model.py
+++ ml/model.py
@@ -5,9 +5,15 @@ from sklearn.linear_model import LinearRegression
 current_cost = np.array([[1000], [2000], [3000], [4000], [5000]])
 next_month_cost = np.array([1080, 2150, 3150, 4300, 5500])
 
+# Train model
 model = LinearRegression()
 model.fit(current_cost, next_month_cost)
 
 def predict_cost(new_cost):
+    # input validation added
+    if new_cost <= 0:
+        return 0
+
     prediction = model.predict([[new_cost]])
+
     return float(prediction[0])
