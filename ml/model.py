ed9091affefeec49bb3916a87459dbb848a3cb46
hiteshkumar54 <hitesh.gla_cs.aiml24@gla.ac.in>
2026-03-16 22:10:37 +0530

    Updated app.py for deployment and prediction improvements

--- ml/model.py
+++ ml/model.py
@@ -4,7 +4,7 @@ from sklearn.linear_model import LinearRegression
 # load dataset
 data = pd.read_csv("ml/cloud_cost.csv")
 
-# scale small values better
+# use dataset columns
 X = data[['total_net_cost']]
 y = data['total_projected_monthly_cost']
 
@@ -12,11 +12,5 @@ model = LinearRegression()
 model.fit(X, y)
 
 def predict_cost(new_cost):
-
-    # 👉 adjust small demo inputs
-    if new_cost < 500:
-        new_cost = new_cost * 5
-
     prediction = model.predict([[new_cost]])
-
     return float(prediction[0])
